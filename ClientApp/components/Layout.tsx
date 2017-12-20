c5eba6e3af5ba197d4decefe08d38699e791d82d
boldtbanan <img4@cornell.edu>
2017-12-20 16:32:29 -0600

    wire up nav toggle for small screens

--- ClientApp/components/Layout.tsx
+++ ClientApp/components/Layout.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { NavMenu } from './NavMenu';
+import { connect } from 'react-redux';
+import NavMenu from './NavMenu';
+import * as Nav from '../store/Nav';
 
-export class Layout extends React.Component<{}, {}> {
+class LayoutComponent extends React.Component<any, any> {
   public render() {
+    const { navState, toggleNav } = this.props;
+
     return (
       <div className="application">
         <div className="row">
           <div className="small-3 columns">
-            <NavMenu />
+            <NavMenu expanded={navState.expanded} toggleNav={toggleNav} />
           </div>
           <div className="small-9 columns">
             {this.props.children}
@@ -17,3 +21,10 @@ export class Layout extends React.Component<{}, {}> {
     );
   }
 }
+
+export const Layout = connect((state) => {
+    return {
+      navState: state.nav
+    };
+  },
+  { toggleNav: Nav.actionCreators.toggleNav })(LayoutComponent);
--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -1,38 +1,52 @@
 import * as React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import * as classNames from 'classnames';
+import * as NavStore from '../store/Nav';
 
-export class NavMenu extends React.Component<{}, {}> {
+
+type NavProps =
+  NavStore.NavState
+  & typeof NavStore.actionCreators;
+
+export default class NavMenu extends React.Component<NavProps, {}> {
   public render() {
-    return (
-      <div className='main-nav'>
-        <div className='navbar navbar-inverse'>
-          <div className='top-bar'>
-            <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-              <span className='hide-for-medium'>Toggle navigation</span>
-              <span className='icon-bar'></span>
-              <span className='icon-bar'></span>
-              <span className='icon-bar'></span>
-            </button>
-            <Link className='navbar-brand' to={'/'}>aspdotnet_react_redux_starter_template</Link>
-          </div>
-          <div className='clearfix'></div>
+    const { expanded, toggleNav } = this.props;
 
-          <div className='show-for-medium'>
-            <ul className='vertical menu'>
+    return (
+      <div className={classNames({'main-nav': true, expanded})}>
+        <div className="navbar navbar-inverse">
+          <div className="top-bar">
+            <div className="top-bar-left">
+              <Link to="/">aspdotnet_react_redux_starter_template</Link>
+            </div>
+            <div className="top-bar-right">
+              <ul className="menu">
+                <li>
+                  <button type="button" className="button toggle-menu" onClick={toggleNav}>
+                    <i className="fa fa-bars"></i>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="clearfix"></div>
+
+          <div className="collapsible-menu">
+            <ul className="vertical menu">
               <li>
-                <NavLink exact to={'/'} activeClassName='active'>
-                  <i className='fa fa-fw fa-lg fa-home'></i> Home
-                            </NavLink>
+                <NavLink exact to="/" activeClassName="active">
+                  <i className="fa fa-fw fa-lg fa-home"></i> Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to={'/counter'} activeClassName='active'>
-                  <i className='fa fa-fw fa-lg fa-graduation-cap'></i> Counter
-                            </NavLink>
+                <NavLink to="/counter" activeClassName="active">
+                  <i className="fa fa-fw fa-lg fa-graduation-cap"></i> Counter
+                </NavLink>
               </li>
               <li>
-                <NavLink to={'/fetchdata'} activeClassName='active'>
-                  <i className='fa fa-fw fa-lg fa-th-list'></i> Fetch data
-                            </NavLink>
+                <NavLink to="/fetchdata" activeClassName="active">
+                  <i className="fa fa-fw fa-lg fa-th-list"></i> Fetch data
+                </NavLink>
               </li>
             </ul>
           </div>
